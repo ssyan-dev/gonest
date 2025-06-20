2115650509115f9656182966fa3c3e16381e3562
Stanislav Simakhin <me@ssyan.ru>
2025-06-20 05:09:59 +0300

    init project: create folder, init git, init go module

--- internal/commands/init/init_project.go
+++ internal/commands/init/init_project.go
@@ -1,6 +1,10 @@
 package init_project
 
-import "log"
+import (
+	"log"
+
+	"github.com/ssyan-dev/gonest/internal/utils"
+)
 
 func HandleInitProject(args []string) {
 	project := ""
@@ -9,4 +13,31 @@ func HandleInitProject(args []string) {
 	}
 
 	log.Printf("Initializing project \"%s\"...", project)
+
+	if err := utils.CreateFolderIfNotExists(project); err == nil {
+		log.Fatal("ERROR: project already exists")
+	}
+
+	if checkIsGitExists() {
+		cmdStr := "git init"
+		if err := utils.ExecuteCommand(cmdStr, cmdStr, project); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	initGoStr := "go mod init " + project
+	if err := utils.ExecuteCommand(initGoStr, initGoStr, project); err != nil {
+		log.Fatal(err)
+	}
+
+	utils.ClearTerminal()
+	log.Printf("Project \"%s\" initialized.", project)
+}
+
+func checkIsGitExists() bool {
+	cmdStr := "git --version"
+	if err := utils.ExecuteCommand(cmdStr, cmdStr, ""); err == nil {
+		return true
+	}
+	return false
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -6,17 +6,31 @@ import (
 	"runtime"
 )
 
-func ClearTerminal() error {
+func ExecuteCommand(windowsCmd, unixCmd, dir string) error {
 	var cmd *exec.Cmd
 
 	switch runtime.GOOS {
 	case "windows":
-		cmd = exec.Command("cls")
+		cmd = exec.Command("cmd", "/C", windowsCmd)
 	default:
-		cmd = exec.Command("clear")
+		cmd = exec.Command("sh", "-c", unixCmd)
 	}
-	
+	if dir != "" {
+		cmd.Dir = dir
+	}
+
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
+}
+
+func ClearTerminal() error {
+	return ExecuteCommand("cls", "clear", "")
+}
+
+func CreateFolderIfNotExists(dir string) error {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return os.MkdirAll(dir, os.ModePerm)
+	}
+	return nil
 }
